7269f3b72d5b54c346cc7fe8985d89d0867ed743
eaxxe <eaxxeg@gmail.com>
2026-03-22 15:13:16 +0300

    feat: add sign-up form into page
    Why:
    Реализовать форму регистрации для новых пользователей, чтобы они могли создавать аккаунты и получать доступ к функционалу сайта. Это необходимо для расширения пользовательской базы и улучшения взаимодействия с сервисом.
    
    What:
    - Создан компонент SignUpPage, который содержит форму регистрации.
    - Форма включает поля для ввода имени, электронной почты и пароля.
    - Добавлена базовая валидация для полей формы (например, проверка на пустые значения и формат электронной почты).
    - Подключены стили для улучшения внешнего вида формы.
    
    Notes:
    - В настоящее время форма не отправляет данные на сервер, это будет реализовано в следующем этапе разработки.

--- src/pages/auth/SignUpPage/SignUpPage.tsx
+++ src/pages/auth/SignUpPage/SignUpPage.tsx
@@ -1,9 +1,10 @@
 import { AuthLayout } from "@/components/layout"
+import { SignUpForm } from "@/components/features/auth"
 
 export const SignUpPage = () => {
 	return (
 		<AuthLayout>
-			<div>SignInPage</div>
+			<SignUpForm></SignUpForm>
 		</AuthLayout>
 	)
 }

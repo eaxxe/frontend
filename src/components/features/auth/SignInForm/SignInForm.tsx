730820042847beacef36e185e33a6915a095f130
eaxxe <eaxxeg@gmail.com>
2026-03-22 14:46:35 +0300

    feat: use UI components into auth forms
    Why:
    Ранее в формах использовались нативные input, что не соответствовало дизайну и не обеспечивало единообразия в интерфейсе. Теперь формы используют общие UI компоненты, что улучшает внешний вид и упрощает поддержку кода.
    
    What:
    - Заменены нативные input на кастомные UI компоненты в SignInForm и SignUpForm
    - Добавлены необходимые стили для новых компонентов
    
    Notes:
    Старое поведение сохранено, формы по-прежнему работают как раньше, но теперь выглядят лучше и соответствуют дизайну приложения.

--- src/components/features/auth/SignInForm/SignInForm.tsx
+++ src/components/features/auth/SignInForm/SignInForm.tsx
@@ -1,5 +1,36 @@
+import { useTranslation } from 'react-i18next'
+import { Input, Button } from '@/components/shared/ui'
+
 export const SignInForm = () => {
+	const { t } = useTranslation('auth')
+
 	return (
-		<div>SignInForm</div>
+		<div>
+			<h1>{t('signIn')}</h1>
+
+			<form onSubmit={(e) => e.preventDefault()}>
+				<Input
+					type="email"
+					placeholder={t('email')}
+				/>
+
+				<Input
+					type="password"
+					placeholder={t('password')}
+				/>
+
+				<p>
+					{t('forgotPassword?')}
+				</p>
+
+				<p>
+					{t('noAccount?')} <a href="/signin">{t('signUp')}</a>
+				</p>
+
+				<Button type="submit">
+					{t('signIn')}
+				</Button>
+			</form>
+		</div>
 	)
 }
--- src/components/features/auth/SignUpForm/SignUpForm.tsx
+++ src/components/features/auth/SignUpForm/SignUpForm.tsx
@@ -1,5 +1,48 @@
+import { useTranslation } from 'react-i18next'
+import { Input, Button } from '@/components/shared/ui'
+
 export const SignUpForm = () => {
+	const { t } = useTranslation('auth')
+
 	return (
-		<div>SignUpForm</div>
+		<div>
+			<h1>{t('signUp')}</h1>
+
+			<form onSubmit={(e) => e.preventDefault()}>
+				<Input
+					type="email"
+					placeholder={t('email')}
+				/>
+
+				<Input
+					type="text"
+					placeholder={t('code')}
+					rightElement={
+						<Button type="button" styleType="sendCode">
+							{t('sendCode')}
+						</Button>
+					}
+				/>
+
+				<Input
+					type="password"
+					placeholder={t('password')}
+					isPassword
+				/>
+
+				<Input
+					type="password"
+					placeholder={t('confirmPassword')}
+				/>
+
+				<p>
+					{t('alreadyHaveAnAccount?')} <a href="/signin">{t('signIn')}</a>
+				</p>
+
+				<Button type="submit">
+					{t('signUp')}
+				</Button>
+			</form>
+		</div>
 	)
 }
